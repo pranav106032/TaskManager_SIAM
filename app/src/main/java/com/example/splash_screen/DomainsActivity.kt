c335c7d221b2c01dc0cde61497c5fa0ebae141d7
Eddy <adigupta239@gmail.com>
2021-10-07 23:10:47 +0530

    made changes to the login screen and added the navigation drawer

--- app/src/main/java/com/example/splash_screen/DomainsActivity.kt
+++ app/src/main/java/com/example/splash_screen/DomainsActivity.kt
@@ -3,13 +3,19 @@ package com.example.splash_screen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import kotlinx.android.synthetic.main.activity_domains.*
+import kotlinx.android.synthetic.main.drawer_layout.*
 
 class DomainsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_domains)
-        supportActionBar!!.title = "Domains"
+        setContentView(R.layout.drawer_layout)
+        setSupportActionBar(toolbar3)
+        val toggle = ActionBarDrawerToggle(this,drawer,toolbar3,R.string.open,R.string.close)
+        toggle.isDrawerIndicatorEnabled = true
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
         card.setOnClickListener {
             startActivity(Intent(this,AppDomain::class.java))
         }
